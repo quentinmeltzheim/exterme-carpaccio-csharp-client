678adbc85084424d8c0e9807afa5e149c7df5463
quentin.meltzheim <quentin_mel@hotmail.fr>
2016-06-16 17:04:16 +0200

    Initialisation du projet de test - Creation du bat

---
+++ testing/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using xCarpaccio.client;
+
+namespace testing
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void newBillReturn0IfOrderIsNull()
+        {
+            Bill bill = new Bill(null);
+         
+        }
+    }
+}
